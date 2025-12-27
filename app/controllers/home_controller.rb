a4c3146b633da1a03e28c4636c4286a42842dc0d
ryo-ri-16 <ujiro1998@gmail.com>
2025-12-27 22:18:15 +0900

    Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, only: :index
 
   def index
+    @today_lists = current_user.lists.scheduled_today.order(scheduled_time: :asc).includes(:category, :list_items)
+    @near_lists = current_user.lists.near_future.order(scheduled_on: :asc, scheduled_time: :asc).includes(:category, :list_items)
+    @past_lists = current_user.lists.past_not_complete.order(scheduled_on: :desc, scheduled_time: :desc).includes(:category, :list_items)
   end
 
   def welcome
--- vendor/bundle/ruby/3.2.0/gems/tailwindcss-ruby-4.1.16-arm64-darwin/lib/tailwindcss/ruby/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Tailwindcss
-  module Ruby
-    VERSION = "4.1.16"
-  end
-end

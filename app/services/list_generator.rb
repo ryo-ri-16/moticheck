1d219fad049abd14c2932a984f9a2f2983d2851b
ryo-ri-16 <ujiro1998@gmail.com>
2026-03-01 09:48:18 +0900

    修正利用時間

--- app/services/list_generator.rb
+++ app/services/list_generator.rb
@@ -30,7 +30,7 @@ class ListGenerator
       category: template.category,
       list_template: template,
       target_date: date,
-      scheduled_at: date.to_time.change(hour: 8)
+      scheduled_at: today.in_time_zone.change(hour: 8)
     )
 
     template.list_template_items.each do |template_item|
